7203de7e4b8283f894882806c4baccc102ba7234
jim.rush <jimmy_rush@hotmail.com>
2019-07-17 18:12:44 -0500

    Kinda starting over.

--- src/main/java/com/rushware/library/Controller.java
+++ src/main/java/com/rushware/library/Controller.java
@@ -1,5 +1,6 @@
 package com.rushware.library;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,12 @@ import java.util.List;
 @RestController
 public class Controller {
 
-  @RequestMapping("/books")
+  @GetMapping("/")
+  public String home() {
+    return "It works.";
+  }
+
+  @GetMapping("/api/books")
   public List<Book> getBooks() {
 
     Library library = new Library();
